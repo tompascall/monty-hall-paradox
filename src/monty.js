fbcaca54e45f2ccab8c3b9539601eff6cbd2342b
tompascall <tompascall@gmail.com>
2015-03-12 15:44:30 +0100

    fixing...

--- src/monty.js
+++ src/monty.js
@@ -5,30 +5,79 @@
 var readline = require('readline');
 
 var monty = {
+  doors: [],
   setDoorNumbers: function (getAnswer) {
     monty.doorNumbers = getAnswer('Please give me the number of doors: ');
     if (typeof monty.doorNumbers !== 'number') {
       monty.doorNumbers = Number(monty.doorNumbers);
     }
   },
+
   setRoundNumbers: function (getAnswer) {
     monty.roundNumbers = getAnswer('Please give me the number of rounds: ');
     if (typeof monty.roundNumbers !== 'number') {
       monty.roundNumbers = Number(monty.roundNumbers);
     }
   },
+
   setMethod: function (getAnswer) {
     monty.method = getAnswer('Shall we stay or shall we change the door at the end? (stay: 0, change: 1) ');
     if (Number(monty.method) === 0) {
       monty.method = 'stay';
     }
     else monty.method = 'change';
+  },
+
+  initDoors: function () {
+    for (var i = 0; i < monty.doorNumbers; i++) {
+      monty.doors.push('goat');
+    }
+    monty.carPosition = getRandomIntNumber(0, monty.doorNumbers - 1);
+    monty.doors[monty.carPosition] = 'car';
+  },
+
+  chooseDoor: function () {
+    monty.chosenDoor = getRandomIntNumber(0, monty.doorNumbers - 1);
+  },
+
+  getResult: function () {
+    if ((monty.carPosition === monty.chosenDoor) && monty.method === 'stay') {
+      return 'win';
+    }
+    if ((monty.carPosition === monty.chosenDoor) && monty.method === 'change') {
+      return 'lose';
+    }
+    if ((monty.carPosition !== monty.chosenDoor) && monty.method === 'change') {
+      return 'win';
+    }
+    if ((monty.carPosition !== monty.chosenDoor) && monty.method === 'stay') {
+      return 'lose';
+    }
+  },
+
+  showResult: function () {
+    var wins = 0;
+    var loses = 0;
+    var result;
+    for (var i = 1; i <= monty.roundNumbers; i++) {
+      result = monty.getResult();
+      if (result === 'win') {
+        wins++;
+      }
+      else {
+        loses++;
+      }
+    }
+    console.log(wins / loses);
   }
 };
 
 monty.setDoorNumbers(getAnswer);
-monty.setRoundNumbers(getAnswer);
-monty.setMethod(getAnswer);
+// monty.setRoundNumbers(getAnswer);
+// monty.setMethod(getAnswer);
+// monty.initDoors();
+// monty.chooseDoor();
+// monty.showResult();
 
 
 function getAnswer(question) {
@@ -44,5 +93,9 @@ function getAnswer(question) {
     });
     return answer;
   }
+
+function getRandomIntNumber (min, max) {
+  return Math.floor(Math.random() * (max - min + 1) + min);
+}
 
 module.exports = monty;
--- test/monty.spec.js
+++ test/monty.spec.js
@@ -5,17 +5,15 @@
 var expect = require('expect.js');
 var monty = require('../src/monty.js');
 var readline = require('readline');
-var userAnswer;
 
-describe('test Monty Hall paradox', function () {
-  it('should be ok', function () {
-    expect(monty).to.be.an('object');
-  });
+
+describe('Get user inputs', function () {
+  var userAnswer;
 
   it('should set the number of doors', function (done) {
-    userAnswer = '1\n';
+    userAnswer = '3\n';
     monty.setDoorNumbers(getAnswer.bind(null, done));
-    expect(monty.doorNumbers).to.equal(1);
+    expect(monty.doorNumbers).to.equal(3);
   });
 
   it('should set the number of rounds', function (done) {
@@ -24,11 +22,11 @@ describe('test Monty Hall paradox', function () {
     expect(monty.roundNumbers).to.equal(2);
   });
 
-  it('should set the calculation method to "stay"', function (done) {
-    userAnswer = '0\n';
-    monty.setMethod(getAnswer.bind(null, done));
-    expect(monty.method).to.equal('stay');
-  });
+   it('should set the calculation method to "stay"', function (done) {
+     userAnswer = '0\n';
+     monty.setMethod(getAnswer.bind(null, done));
+     expect(monty.method).to.equal('stay');
+   });
 
   it('should set the calculation method to "change"', function (done) {
     userAnswer = '1\n';
@@ -56,4 +54,18 @@ describe('test Monty Hall paradox', function () {
     mockUserAnswer(rl, userAnswer);
     return answer;
   }
+});
+
+describe('Prepare the room behind the doors', function () {
+
+  it('should fill the rooms behind the doors', function () {
+    monty.initDoors();
+    expect(monty.doors.length).to.equal(monty.doorNumbers);
+  });
+
+  it('should put a car behind one of the doors', function () {
+    monty.initDoors();
+    expect(monty.doors.indexOf('car')).to.be.greaterThan(-1);
+    expect(monty.doors.indexOf('car')).to.be.lessThan(monty.doorNumbers);
+   });
 });
