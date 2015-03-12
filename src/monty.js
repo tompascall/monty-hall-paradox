0a5c246755705e951af1eff9f06bfd41d3095473
tompascall <tompascall@gmail.com>
2015-03-12 11:29:05 +0100

    set door numbers

--- src/monty.js
+++ src/monty.js
@@ -1,11 +1,38 @@
 // monty.js
 
 'use strict';
+//var Q = require('q');
+var readline = require('readline');
+//var deferred = Q.defer();
 
 var monty = {
+  doorNumbers: 2,
   setDoorNumbers: function(getAnswer) {
-    monty.doorNumbers = getAnswer('Please give me the number of the doors');
+
+    monty.doorNumbers = getAnswer('Please give me the number of the doors: ');
+    if (typeof monty.doorNumbers !== 'number') {
+      monty.doorNumbers = Number(monty.doorNumbers);
+    }
   }
 };
+
+monty.setDoorNumbers(getAnswer);
+
+function getAnswer(question) {
+  var answ;
+  var rl = readline.createInterface({
+    input: process.stdin,
+    output: process.stdout
+  });
+
+  rl.question(question, function(answer) {
+    console.log(answer);
+    answ = answer;
+    rl.close();
+  });
+
+  return answ;//deferred.promise;
+}
+
 
 module.exports = monty;
--- test/monty.spec.js
+++ test/monty.spec.js
@@ -4,19 +4,32 @@
 
 var expect = require('expect.js');
 var monty = require('../src/monty.js');
+var readline = require('readline');
 
 describe('test Monty Hall paradox', function () {
   it('should be ok', function () {
     expect(monty).to.be.an('object');
   });
 
-  it('should get the number of doors', function () {
-    var answer = 1;
-    var getAnswer = function (question) {
-       console.log(question);
-       return answer;
-    };
-    monty.setDoorNumbers(getAnswer);
+  it('should set the number of doors', function (done) {
+    monty.setDoorNumbers(getAnswer.bind(null, done));
     expect(monty.doorNumbers).to.equal(1);
   });
+
+  function getAnswer(done, question) {
+    var answer;
+    var rl = readline.createInterface({
+      input: process.stdin,
+      output: process.stdout
+    });
+
+    rl.question(question, function(answ) {
+      answer = answ;
+      rl.close();
+      done();
+    });
+
+    rl.write('1\n');
+    return answer;
+  }
 });
