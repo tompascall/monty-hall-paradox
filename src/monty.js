109fc0d26d87b4432d7698edf5234ec74d8ff8df
tompascall <tompascall@gmail.com>
2015-03-12 13:15:51 +0100

    get user inputs

--- src/monty.js
+++ src/monty.js
@@ -1,38 +1,48 @@
 // monty.js
 
 'use strict';
-//var Q = require('q');
+
 var readline = require('readline');
-//var deferred = Q.defer();
 
 var monty = {
-  doorNumbers: 2,
-  setDoorNumbers: function(getAnswer) {
-
-    monty.doorNumbers = getAnswer('Please give me the number of the doors: ');
+  setDoorNumbers: function (getAnswer) {
+    monty.doorNumbers = getAnswer('Please give me the number of doors: ');
     if (typeof monty.doorNumbers !== 'number') {
       monty.doorNumbers = Number(monty.doorNumbers);
     }
+  },
+  setRoundNumbers: function (getAnswer) {
+    monty.roundNumbers = getAnswer('Please give me the number of rounds: ');
+    if (typeof monty.roundNumbers !== 'number') {
+      monty.roundNumbers = Number(monty.roundNumbers);
+    }
+  },
+  setMethod: function (getAnswer) {
+    monty.method = getAnswer('Shall we stay or shall we change the door at the end? (stay: 0, change: 1) ');
+    if (Number(monty.method) === 0) {
+      monty.method = 'stay';
+    }
+    else monty.method = 'change';
   }
 };
 
 monty.setDoorNumbers(getAnswer);
+monty.setRoundNumbers(getAnswer);
+monty.setMethod(getAnswer);
+
 
 function getAnswer(question) {
-  var answ;
-  var rl = readline.createInterface({
-    input: process.stdin,
-    output: process.stdout
-  });
+    var answer;
+    var rl = readline.createInterface({
+      input: process.stdin,
+      output: process.stdout
+    });
 
-  rl.question(question, function(answer) {
-    console.log(answer);
-    answ = answer;
-    rl.close();
-  });
-
-  return answ;//deferred.promise;
-}
-
+    rl.question(question, function(answ) {
+      answer = answ;
+      rl.close();
+    });
+    return answer;
+  }
 
 module.exports = monty;
--- test/monty.spec.js
+++ test/monty.spec.js
@@ -5,6 +5,7 @@
 var expect = require('expect.js');
 var monty = require('../src/monty.js');
 var readline = require('readline');
+var userAnswer;
 
 describe('test Monty Hall paradox', function () {
   it('should be ok', function () {
@@ -12,9 +13,32 @@ describe('test Monty Hall paradox', function () {
   });
 
   it('should set the number of doors', function (done) {
+    userAnswer = '1\n';
     monty.setDoorNumbers(getAnswer.bind(null, done));
     expect(monty.doorNumbers).to.equal(1);
   });
+
+  it('should set the number of rounds', function (done) {
+    userAnswer = '2\n';
+    monty.setRoundNumbers(getAnswer.bind(null, done));
+    expect(monty.roundNumbers).to.equal(2);
+  });
+
+  it('should set the calculation method to "stay"', function (done) {
+    userAnswer = '0\n';
+    monty.setMethod(getAnswer.bind(null, done));
+    expect(monty.method).to.equal('stay');
+  });
+
+  it('should set the calculation method to "change"', function (done) {
+    userAnswer = '1\n';
+     monty.setMethod(getAnswer.bind(null, done));
+    expect(monty.method).to.equal('change');
+  });
+
+  function mockUserAnswer (readLine, mockAnswer) {
+    readLine.write(mockAnswer);
+  }
 
   function getAnswer(done, question) {
     var answer;
@@ -29,7 +53,7 @@ describe('test Monty Hall paradox', function () {
       done();
     });
 
-    rl.write('1\n');
+    mockUserAnswer(rl, userAnswer);
     return answer;
   }
 });
